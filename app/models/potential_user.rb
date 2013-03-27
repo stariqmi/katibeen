3ebe3474f7a8bc491a189f70be1af5d717329010
Salman <salman@ubuntu.ubuntu-domain>
2013-03-26 23:00:54 -0400

    email disabled

--- app/models/potential_user.rb
+++ app/models/potential_user.rb
@@ -1,7 +1,7 @@
 class PotentialUser < ActiveRecord::Base
   attr_accessible :email, :url, :timezone
   before_save { |puser| puser.email = email.downcase }
-  after_save :sendWelcomeEmail
+  #after_save :sendWelcomeEmail
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :url, presence: true
