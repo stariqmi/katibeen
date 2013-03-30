54a3b45ab1216273cdf04e3331a9153b0b93bc11
Salman Tariq Mirza <stariqmi@gmail.com>
2013-03-30 19:33:49 -0400

    model changes

--- app/models/outgoing_day_prayer.rb
+++ app/models/outgoing_day_prayer.rb
@@ -1,4 +1,4 @@
 class OutgoingDayPrayer < ActiveRecord::Base
-  attr_accessible :asr, :fajr, :isha, :maghrib, :time, :url, :user_id, :weekday, :zuhr, :status
+  attr_accessible :asr, :fajr, :isha, :maghrib, :time, :url, :user_id, :weekday, :zuhr, :status, :average, :total_prayed
   belongs_to :user
 end
