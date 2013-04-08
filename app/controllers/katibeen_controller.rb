25d787d57e0493907d24d6559fa80473dc18e98c
Farhan Munshi <farhanm995@gmail.com>
2013-04-07 20:34:19 -0400

    heroku

--- app/controllers/katibeen_controller.rb
+++ app/controllers/katibeen_controller.rb
@@ -308,7 +308,7 @@ include UserPerformanceDataHelper # To generate missed prayers data for a user
       @result = "Something went wrong"
       redirect_to :action => "home"
     else
-      if dataCount == 1 
+      if dataCount == 1 or dataCount == 2
         dayData.update_attribute(:average, counter)
       elsif dataCount < 15
         avg = (counter + data[dataCount - 2].total_prayed) / dataCount.to_f
