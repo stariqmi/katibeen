b709bd03b9599b5ca55a40343704a54f54f389af
fm2munsh <farhanm995@gmail.com>
2013-04-26 15:20:33 -0400

    Server setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.mandrillapp.com",
-  :port                 => 587,
-  :domain 							=> 'katibean.com',
-	:enable_starttls_auto => true,
-	:user_name            => "salah@katibean.com",
-	:password             => 'Ncwby9GTNOkK30io1WSOnA',
-	:authentication 			=> 'login',
+	:address              	=> "smtp.mandrillapp.com",
+	:port                 	=> 587,
+	:domain 				=> 'katibean.com',
+	:enable_starttls_auto 	=> true,
+	:user_name            	=> "salah@katibean.com",
+	:password             	=> 'Ncwby9GTNOkK30io1WSOnA',
+	:authentication 		=> 'login',
 }
 
 ActionMailer::Base.delivery_method = :smtp
