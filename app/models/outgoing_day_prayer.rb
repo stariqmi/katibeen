15f0817c7fc1a5cd3a6e483ee055a25df3dd3685
fm2munsh <farhanm995@gmail.com>
2013-05-01 17:40:27 -0400

    Fixed spreadsheet averages

--- app/models/outgoing_day_prayer.rb
+++ app/models/outgoing_day_prayer.rb
@@ -1,4 +1,10 @@
 class OutgoingDayPrayer < ActiveRecord::Base
   attr_accessible :asr, :fajr, :isha, :maghrib, :time, :url, :user_id, :weekday, :zuhr, :status, :average, :total_prayed, :created_at, :updated_at
   belongs_to :user
+
+  def get_average 
+  	average = self.asr + self.fajr + self. maghrib + self.isha + self.zuhr 
+  	average = average/2
+  	return average
+  end
 end
